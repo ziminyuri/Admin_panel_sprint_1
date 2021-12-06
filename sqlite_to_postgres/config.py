751ae6aeec47d585f87766622ace8a784490a03b
ziminyuri <zimin.yuri22@gmail.com>
2021-12-06 11:20:35 +0700

    Виртуальное окружение, логгирование, вынес dataclasses в models.py

---
+++ sqlite_to_postgres/config.py
@@ -0,0 +1,7 @@
+import logging
+import environ
+
+logging.basicConfig(filename="apps.log", level=logging.INFO)
+
+env = environ.Env()
+environ.Env.read_env('.env')
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,128 +1,13 @@
 import sqlite3
 import sys
-import uuid
 from dataclasses import dataclass
-from datetime import datetime
 
 import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
-
-@dataclass(frozen=True)
-class FilmWork:
-    __slots__ = ['id', 'title', 'description', 'creation_date', 'certificate', 'file_path', 'rating', 'type',
-                 'created_at', 'updated_at']
-
-    id: uuid
-    title: str
-    description: str
-    creation_date: str
-    certificate: str
-    file_path: str
-    rating: float
-    type: str
-    created_at: datetime
-    updated_at: datetime
-
-    def get_values(self) -> tuple:
-        return (self.id, self.title, self.description, self.creation_date, self.certificate, self.file_path,
-                self.rating, self.type, self.creation_date, self.updated_at)
-
-    @staticmethod
-    def get_fields_name() -> str:
-        return "id, title, description, creation_date, certificate, file_path, rating, type, created_at, updated_at"
-
-    @staticmethod
-    def get_args() -> str:
-        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
-
-
-@dataclass(frozen=True)
-class Genre:
-    __slots__ = ['id', 'name', 'description', 'created_at',  'updated_at']
-
-    id: uuid
-    name: str
-    description: str
-    created_at: datetime
-    updated_at: datetime
-
-    def get_values(self) -> tuple:
-        return (self.id, self.name, self.description, self.created_at, self.updated_at)
-
-    @staticmethod
-    def get_fields_name() -> str:
-        return "id, name, description, created_at, updated_at"
-
-    @staticmethod
-    def get_args() -> str:
-        return "%s, %s, %s, %s, %s"
-
-
-@dataclass(frozen=True)
-class GenreFilmWork:
-    __slots__ = ['id', 'filmwork_id', 'genre_id', 'created_at']
-
-    id: uuid
-    filmwork_id: FilmWork
-    genre_id: Genre
-    created_at: datetime
-
-    def get_values(self) -> tuple:
-        return (self.id, self.filmwork_id, self.genre_id, self.created_at)
-
-    @staticmethod
-    def get_fields_name() -> str:
-        return "id, filmwork_id, genre_id, created_at"
-
-    @staticmethod
-    def get_args() -> str:
-        return "%s, %s, %s, %s"
-
-
-@dataclass(frozen=True)
-class Person:
-    __slots__ = ['id', 'full_name', 'birth_date', 'created_at', 'updated_at']
-
-    id: uuid
-    full_name: str
-    birth_date: str
-    created_at: datetime
-    updated_at: datetime
-
-    def get_values(self) -> tuple:
-        return (self.id, self.full_name, self.birth_date, self.created_at, self.updated_at)
-
-    @staticmethod
-    def get_fields_name() -> str:
-        return "id, full_name, birth_date, created_at, updated_at"
-
-    @staticmethod
-    def get_args() -> str:
-        return "%s, %s, %s, %s, %s"
-
-
-@dataclass(frozen=True)
-class PersonFilmWork:
-    __slots__ = ['id', 'filmwork_id', 'person_id', 'role', 'created_at']
-
-    id: uuid
-    filmwork_id: FilmWork
-    person_id: Person
-    role: str
-    created_at: datetime
-
-    def get_values(self) -> tuple:
-        return (self.id, self.filmwork_id, self.person_id, self.role, self.created_at)
-
-    @staticmethod
-    def get_fields_name() -> str:
-        return "id, filmwork_id, person_id, role, created_at"
-
-    @staticmethod
-    def get_args() -> str:
-        return "%s, %s, %s, %s, %s"
+from models import FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork
+from config import logging, env
 
 
 class PostgresSaver:
@@ -159,7 +44,7 @@ class PostgresSaver:
             cursor.execute(query)
             self.__connection.commit()
         except Exception as e:
-            print(f"Ошибка записи в PostgreSQL:  {e}")
+            logging.exception(f"Ошибка записи в PostgreSQL: {e}")
 
 
 class SQLiteLoader:
@@ -180,9 +65,8 @@ class SQLiteLoader:
 
             return data
         except Exception as e:
-            sys.exit(f"Ошибка чтения из БД Sqlite3: {e}")
-
-
+            logging.exception(f"Ошибка чтения из БД Sqlite3:: {e}")
+            sys.exit()
 
     @staticmethod
     def __get_data_from_table(executor: sqlite3.Cursor, table_name: str, data_class: dataclass) -> list:
@@ -204,12 +88,12 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
 
 if __name__ == '__main__':
     dsl = {
-        'dbname': 'movies_database',
-        'user': 'postgres',
-        'password': 1234,
-        'host': 'localhost',
-        'port': 5432
-    }
+            'dbname': env('DB_NAME'),
+            'user': env('DB_USER'),
+            'password': env('DB_PASSWORD'),
+            'host': env('DB_HOST'),
+            'port': env('DB_PORT')
+        }
 
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
